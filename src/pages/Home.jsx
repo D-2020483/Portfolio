206e84eb16a0412ea62994d5f98e95f502e12d00
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-06-14 00:25:37 +0530

    Pages and Routes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return (
+        <div >
+        {/*In this project devided to several parts*/}
+        {/*Teme Toggle*/}
+        {/*Background Effects*/}
+        {/*Navbar*/}
+        {/*Main Content*/}
+        {/*Footer*/}
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of our application.</p>
+        </div>
+    );
+}
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      // This allows you to use '@' as an alias for the 'src' directory
+      "@":  path.resolve(__dirname, "./src"),
+    },
+  },  
+})
