ff50a7dbaa55c4d07a228e2c71b389d998064739
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-06-16 11:15:33 +0530

    DarkMode_Function

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,42 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
 export const Home = () => {
     return (
-        <div >
+        <div className="min-h-screen bg-background text-foreground overflow-hidden">
         {/*In this project devided to several parts*/}
         {/*Teme Toggle*/}
+        <ThemeToggle/>
+
+
+
+
         {/*Background Effects*/}
+
+
+
+
+
+
         {/*Navbar*/}
+
+
+
+
+
+
         {/*Main Content*/}
+
+
+
+
+
         {/*Footer*/}
+
+
+
+
+
+
         <h1>Welcome to the Home Page</h1>
         <p>This is the home page of our application.</p>
         </div>
